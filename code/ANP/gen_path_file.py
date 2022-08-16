b58a634723405d705e36734e67ffe852d283b113
Little Bug <kpzhangs@gmail.com>
2022-08-16 16:09:16 -0400

    Update gen_path_file.py

--- code/ANP/gen_path_file.py
+++ code/ANP/gen_path_file.py
@@ -1,8 +1,8 @@
 import os,sys
 
 names = []
-for fname in os.listdir('buffy_images'):
-	names.append("buffy_images\\"+fname)
+for fname in os.listdir('images'):
+	names.append("images\\"+fname)
 	
 for i in range(int(sys.argv[1]),int(sys.argv[2])):
 	print(names[i])
